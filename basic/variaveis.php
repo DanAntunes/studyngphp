537afc6e752a1564c2e028749c5b6717d3bf4464
Soren <danilo-258@hotmail.com>
2025-01-14 16:46:47 -0300

    Add example of variable usage and conditional statements in PHP

--- basic/variaveis.php
+++ basic/variaveis.php
@@ -6,6 +6,29 @@
   <title>Curso PHP</title>
 </head>
 <body>
+  <?php
+    // string
+    $nome = 'Danilo';
+    // int
+    $idade = 23;
+    // float
+    $peso = 79.5;
+    // boolean
+    $fumante_sn = false; // true = 1 ou false = vazio
+
+    if($fumante_sn == true){
+      $fumante = 'Sim';
+    } else {
+      $fumante = 'Não';
+    }
+  ?>
+
+  <h1>Ficha Cadstral</h1>
+  <br>
+  <p>Nome: <?= $nome ?></p>
+  <p>Idade: <?= $idade ?></p>
+  <p>Peso: <?= $peso ?></p>
+  <p>Fumante: <?= $fumante ?></p>
  
 </body>
 </html>
