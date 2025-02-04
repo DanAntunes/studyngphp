828002a973b09188be08b43f1a3fc86c66d10c8f
Soren <danilo-258@hotmail.com>
2025-02-04 16:24:34 -0300

    Add HTML file demonstrating string functions in PHP

---
+++ inter/funcoes_string.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../phpstudy/basic/css/php.css">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <h2>Funções String</h2>
+  <?php
+    $texto = "Curso completo de PHP";
+
+    // string to lower
+    echo $texto . "<br>";
+    echo strtolower($texto) . "<br>";
+  ?>
+</body>
+</html>
