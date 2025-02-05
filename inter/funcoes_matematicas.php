d14eac82eee12d8ef14cdb28964aca34cdc2e55a
Soren <danilo-258@hotmail.com>
2025-02-05 16:30:38 -0300

    Add HTML structure to funcoes_matematicas.php

---
+++ inter/funcoes_matematicas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../phpstudy/basic/css/php.css">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <h2>Funções Matématicas</h2>
+  <?php
+  
+  ?>
+</body>
+</html>
