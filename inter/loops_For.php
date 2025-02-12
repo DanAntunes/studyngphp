ab74298a60adc15e1a8b94ea9586f7c545db7c75
Soren <danilo-258@hotmail.com>
2025-02-12 16:04:08 -0300

    Add loops_For.php file with basic HTML structure and for loop syntax demonstration

---
+++ inter/loops_For.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../phpstudy/basic/css/php.css">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <h1>For</h1>
+  <?php
+  // Sintaxe While
+  // while (condicao) {
+  //   # code...
+  // }
+   
+  // Sintaxe do while
+  // do {
+  //   # code...
+  // } while (condicao);
+
+  // Sintaxe For
+  for (vairavel; condicao; incremento) {
+    # code...
+  }
+  ?>  
+</body>
+</html>
