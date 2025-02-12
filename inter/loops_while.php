843aa29be264815e6fd0b7e3e57925e64b1c3a89
Soren <danilo-258@hotmail.com>
2025-02-12 04:59:28 -0300

    Add HTML structure and while loop demonstration in loops_while.php

---
+++ inter/loops_while.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../phpstudy/basic/css/php.css">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <h1>While</h1>
+  <?php
+    $num = 1;
+    //operadores comparação / lógicos
+    echo '--- Inicio do Loop --' . '<br>';
+    while($num < 10) {
+      echo $num . '<br>';
+      $num++;
+    }
+    echo '--- Final do Loop --';
+  ?>  
+</body>
+</html>
