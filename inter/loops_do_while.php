718e62ba5ec5fe90fe50118b1f48399c7f158f26
Soren <danilo-258@hotmail.com>
2025-02-12 15:46:56 -0300

    Add do-while loop demonstration in loops_do_while.php

--- inter/loops_do_while.php
+++ inter/loops_do_while.php
@@ -9,7 +9,19 @@
 <body>
   <h1>Do While</h1>
   <?php
- 
+    $x = 10;
+
+    do {
+      echo 'Entrou no do while...<br>';
+      //continue
+      //break
+    } while ($x < 9);//não executa pois a condição é falsa.
+
+     echo '<br>';
+     while ($x < 9) {
+      echo 'Entrou no while...<br>';
+     }
+
   ?>  
 </body>
 </html>
