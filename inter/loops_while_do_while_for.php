2b44cc3c6e04cdb090e3a8a9e78d49a968573178
Soren <danilo-258@hotmail.com>
2025-02-13 10:50:09 -0300

    Add sample data array to loops_while_do_while_for.php for demonstration

--- inter/loops_while_do_while_for.php
+++ inter/loops_while_do_while_for.php
@@ -10,6 +10,16 @@
   <h1> While | Do While | For</h1>
   <?php
 
+  $registros = [
+    ['titulo' => 'Título notícia 1', 'conteudo' => 'Conteúdo notícia 1'],
+    ['titulo' => 'Título notícia 2', 'conteudo' => 'Conteúdo notícia 2'],
+    ['titulo' => 'Título notícia 3', 'conteudo' => 'Conteúdo notícia 3'],
+    ['titulo' => 'Título notícia 4', 'conteudo' => 'Conteúdo notícia 4'],
+    ['titulo' => 'Título notícia 5', 'conteudo' => 'Conteúdo notícia 5'],
+  ];
+
+  print_r($registros);
+
   ?>  
 </body>
 </html>
