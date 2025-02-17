b3ce9dc346d740df0747a50574088aca8a5f8aed
Soren <danilo-258@hotmail.com>
2025-02-17 18:09:22 -0300

    Add login form to card component in index.php for enhanced user experience

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -30,6 +30,17 @@
           Login
         </div>
      </div>
+     <div class="card-body">
+       <form>
+          <div class="form-group">
+          <input type="email" class="form-control" placeholder="E-mail">
+          </div>
+          <div class="form-group">
+          <input type="password" class="form-control" placeholder="Senha">
+          </div>
+          <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+        </form>
+     </div>
     </section>
    </div>
  </main>
