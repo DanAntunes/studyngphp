6dbae2c235094aac0e592d97289fff5f9bbb0b74
Soren <danilo-258@hotmail.com>
2025-02-17 18:00:55 -0300

    Refactor index.php to improve HTML structure and semantics

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -1,49 +1,18 @@
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Aplicativo para abertura e acompanhamento de chamados">
-    <meta name="author" content="Danilo Antunes">
-
-    <title>App Help Desk</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="./src/app.css">
-  </head>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Aplicativo para abertura e acompanhamento de chamados">
+  <meta name="author" content="Danilo Antunes">
+  <!-- Title -->
+  <title>App Help Desk</title>
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="./src/app.css">
+</head>
 <body>
-   <header>
-    <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-          <img src="./public/image/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-          App Help Desk
-        </a>
-      </nav>
-   </header>
-
-   <main class="container">    
-      <div class="row">
-        <div class="card-login">
-          <div class="card">
-            <div class="card-header">
-              Login
-            </div>
-            
-            <div class="card-body">
-              <form>
-                <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
-                </div>
-                <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha">
-                </div>
-                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
-              </form>
-            </div>
-          </div>
-        </div>
-</main>
+  
 </body>
 </html>
