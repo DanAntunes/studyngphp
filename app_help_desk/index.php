a9735338048bfeeb8be480939badb6dea1967771
Soren <danilo-258@hotmail.com>
2025-02-17 17:44:26 -0300

    Add meta tags for improved SEO and responsiveness in index.php

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -1,6 +1,10 @@
 <html>
   <head>
     <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Aplicativo para abertura e acompanhamento de chamados">
+    <meta name="author" content="Danilo Antunes">
     <title>App Help Desk</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
