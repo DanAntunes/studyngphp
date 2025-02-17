086138bd57d69771610551482629df9cf6d858bb
Soren <danilo-258@hotmail.com>
2025-02-17 17:44:47 -0300

    Add Bootstrap CSS link to index.php for improved styling

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Aplicativo para abertura e acompanhamento de chamados">
     <meta name="author" content="Danilo Antunes">
+
     <title>App Help Desk</title>
 
+    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <style>
