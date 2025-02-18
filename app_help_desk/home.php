b93f3662e48a49437de4f2dec2edf42a7b9b01d9
Soren <danilo-258@hotmail.com>
2025-02-18 03:45:47 -0300

    Refactor home.php to improve HTML structure, add meta tags for SEO, and include viewport settings

--- app_help_desk/home.php
+++ app_help_desk/home.php
@@ -1,15 +1,17 @@
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>App Help Desk</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <style>
-
-    </style>
-  </head>
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Aplicativo para abertura e acompanhamento de chamados">
+  <meta name="author" content="Danilo Antunes">
+  <title>App Help Desk</title>
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="./src/app.css">
+</head>
   <body>
 
     <nav class="navbar navbar-dark bg-dark">
