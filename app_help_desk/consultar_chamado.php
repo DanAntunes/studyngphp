759f7695c4156b6a6062be7ff2f659d839c70b19
Soren <danilo-258@hotmail.com>
2025-02-18 03:43:30 -0300

    Refactor consultar_chamado.php to improve HTML structure, add meta tags for SEO, and include a navigation header

--- app_help_desk/consultar_chamado.php
+++ app_help_desk/consultar_chamado.php
@@ -1,27 +1,29 @@
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>App Help Desk</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <style>
-      .card-consultar-chamado {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Aplicativo para abertura e acompanhamento de chamados">
+  <meta name="author" content="Danilo Antunes">
+  <title>App Help Desk</title>
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="./src/app.css">
+</head>
   <body>
-
+  <!-- Cabeçalho com navegação -->
+  <header>
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        App Help Desk
+        <figure class="mb-0">
+          <img src="./public/image/logo.png" alt="Logo App Help Desk" class="d-inline-block align-top logo">
+        </figure>
+        <span>App Help Desk</span>
       </a>
     </nav>
+  </header>
 
     <div class="container">    
       <div class="row">
