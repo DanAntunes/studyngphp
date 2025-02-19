5161181f9f2d0dd98341227619a5605c79d59694
Soren <danilo-258@hotmail.com>
2025-02-19 04:24:19 -0300

    Add user credentials array to valida_login.php for authentication

--- app_help_desk/src/scripts/valida_login.php
+++ app_help_desk/src/scripts/valida_login.php
@@ -1,4 +1,10 @@
 <?php
+
+  //usuarios do sistema
+  $usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+  );
   // $_GET['email'];
   // $_GET['senha'];
 
