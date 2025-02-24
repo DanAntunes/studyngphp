87687fdfd0e952f6a60debc553e9fd9b3f077242
Soren <danilo-258@hotmail.com>
2025-02-24 12:10:20 -0300

    fix(session): finalize session termination and redirect on logoff
    - Initialize session with session_start()
    - Destroy session with session_destroy() to remove session data
    - Redirect user to "/phpstudy/app_help_desk/index.php" after logoff

--- app_help_desk/src/scripts/logoff.php
+++ app_help_desk/src/scripts/logoff.php
@@ -1,4 +1,15 @@
 <?php
 
+  
+  //remover inidices de array de sessão
+  //unset()
+
+  //destruir a variável de sessão
+  //session_destroy()
+
+  session_start();
+
+  session_destroy();
+  header('Location: /phpstudy/app_help_desk/index.php');
 
 ?>
