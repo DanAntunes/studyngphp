0dd2bf6d82d3732d748f39e215c3abe4478919b3
Soren <danilo-258@hotmail.com>
2025-03-04 02:20:48 -0300

    chore: remover arquivos obsoletos após reorganização de pastas

--- app_help_desk/src/scripts/validador_acesso.php
+++
@@ -1,7 +0,0 @@
-<?php
-  session_start();
-  if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-    header('Location: /phpstudy/app_help_desk/index.php?login=erro2');
-  }
-?>
-
