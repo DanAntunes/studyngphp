d4cb29482a70a24338705555aab295310d262396
Soren <danilo-258@hotmail.com>
2025-03-05 00:45:41 -0300

    feat(example): adicionar script de demonstração de comentários em PHP
    - Exibe comentários de linha utilizando // e #
    - Inclui exemplo de comentário em bloco com /* ... */

---
+++ class/basic/comentarios.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+<body>
+ 
+  <!-- Comentários -->
+  <?php
+    // Comentário de uma linha
+    echo 'Comando echo<br>';
+    
+    # Comentário de uma linha
+    print 'Comando print';
+
+    /* Comentário de múltiplas linhas
+    echo 'Comando echo<br>';
+    echo 'Comando echo<br>';
+    echo 'Comando echo<br>';
+    */
+  ?>
+   
+</body>
+</html>
