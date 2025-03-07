67db7922d61ec784c0912190c2830afe1454e7d6
Soren <danilo-258@hotmail.com>
2025-03-07 00:54:01 -0300

    feat(php): adicionar exemplo de verificação de valores false, null e empty

---
+++ class/inter/false_null_empty.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../phpstudy/basic/css/php.css">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <h1>False | Null | Empty</h1>
+  <?php
+   //false (True ou False) - tipo variavel boolean
+   //null e empty - valores especiais
+   $funcioario1 = null;
+   $funcionario2 = '';
+   $funcionario3 = false;
+
+  //valores null
+  if (is_null($funcioario1)) {
+    echo 'Sim, a variável é null';
+  } else {
+    echo 'Não, a variável não é null';
+  }
+  echo '<br>';
+  if (is_null($funcionario2)) {
+    echo 'Sim, a variável é null';
+  } else {
+    echo 'Não, a variável não é null';
+  }
+  echo '<br>';
+  if (is_null($funcionario3)) {
+    echo 'Sim, a variável é null';
+  } else {
+    echo 'Não, a variável não é null';
+  }
+
+  echo '<br>';
+  echo '<hr>';
+
+  //valores empty
+  if (empty($funcioario1)) {
+    echo 'Sim, a variável é empty';
+  } else {
+    echo 'Não, a variável não é empty';
+  }
+  echo '<br>';
+  if (empty($funcionario2)) {
+    echo 'Sim, a variável é empty';
+  } else {
+    echo 'Não, a variável não é empty';
+  }
+  echo '<br>';
+  if (empty($funcionario3)) {
+    echo 'Sim, a variável é empty';
+  } else {
+    echo 'Não, a variável não é empty';
+  }
+  ?>  
+</body>
+</html>
