7895ba62f72ab56f3fd32b95db3fb46be3d3336f
Soren <danilo-258@hotmail.com>
2025-03-11 08:01:32 -0300

    refactor(auth): atualizar redirecionamento em validador_acesso.php para nova estrutura de diretórios

--- app/app_help_desk/src/scripts/validador_acesso.php
+++ app/app_help_desk/src/scripts/validador_acesso.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-    header('Location: /phpstudy/app_help_desk/index.php?login=erro2');
+    header('Location: /phpstudy/app/app_help_desk/index.php?login=erro2');
   }
 ?>
 
