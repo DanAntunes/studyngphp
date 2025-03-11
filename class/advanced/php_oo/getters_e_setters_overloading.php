0e9d5c1a261fdac0ce47a0a9b7aa3c4fe1ace525
Soren <danilo-258@hotmail.com>
2025-03-11 07:39:36 -0300

    feat(POO): adicionar exemplo de getters e setters com métodos mágicos
    - Define a classe Funcionario com atributos dinâmicos
    - Implementa os métodos __set e __get para manipulação interna de propriedades
    - Demonstra a utilização dos métodos mágicos para acessar e modificar atributos

---
+++ class/advanced/php_oo/getters_e_setters_overloading.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../php.css">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <h1>POO | Getters e Setters Overloading </h1>
+  <?php 
+
+  // modelo
+  class Funcionario {
+
+    //atributos
+    public $nome = null;
+    public $telefone = null;
+    public $numFilhos = null;
+    public $cargo = null;
+    public $salario = null;
+
+    //getters setters (overloading / sobrecarga)
+    function __set($atributo, $valor) {
+      $this->$atributo = $valor;
+    }
+
+    function __get($atributo) {
+      return $this->$atributo;
+    }
+
+
+    //getters and setters
+    // function setNome($nome){
+    //   $this->nome = $nome;
+    // }
+
+    // function setTelefone($telefone){
+    //   $this->telefone = $telefone;
+    // }
+
+    // function setNumFilhos($numFilhos){
+    //   $this->numFilhos = $numFilhos;
+    // }
+
+    // function getNome(){
+    //   return $this->nome;
+    // }
+
+    // function getTelefone(){
+    //   return $this->telefone;
+    // }
+
+    // function getNumFilhos(){
+    //   return $this->numFilhos;
+    // }
+
+    //métodos
+    function resumirCadFunc() {
+      return "$this->nome possui $this->numFilhos filhos. 
+      Numero para contato : $this->telefone";
+    }
+
+    function modificarNumFilhos($numFilhos){
+      //afetar um atributo do objeto
+      $this->numFilhos = $numFilhos;
+    }
+
+  }
+
+  $y = new Funcionario();
+  $y->__set('nome', 'Danilo');
+  $y->__set('numFilhos', 2);
+  $y->__set('telfone', '71 985092511');
+  //echo $y->resumirCadFunc();
+  echo $y->__get('nome') . ' possui ' .  $y->__get('numFilhos'). ' filho(s). ' . 
+  'Numero para contato :' . $y->__get('telefone');
+
+  // echo '<br>';
+  // $x = new Funcionario();
+  // $x->setNome('Soren');
+  // $x->setNumFilhos (0);
+  // $x->setTelefone('40028922');
+  // echo $x->resumirCadFunc();
+
+
+
+  ?>
+
+</body>
+</html>
