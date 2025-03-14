34fb17a3aaebc1e428c73a8d26233c6ec4434797
Soren <danilo-258@hotmail.com>
2025-03-14 06:59:03 -0300

    feat(interfaces): adicionar exemplos de interfaces e múltiplas implementações
    - Define EquipamentoEletronicoInterface e implementa em Geladeira e TV
    - Adiciona interfaces MamiferoInterface, TerrestreInterface e AquaticoInterface, implementadas em Humano e Baleia
    - Cria interfaces AnimalInterface e AveInterface (que estende AnimalInterface) e implementa na classe Papagaio
    - Demonstra o uso de métodos específicos em cada implementação

--- class/advanced/php_oo/interface.php
+++ class/advanced/php_oo/interface.php
@@ -54,7 +54,80 @@
 
   echo '<hr>';
 
+  interface MamiferoInterface {
+    public function respirar();
+  }
+
+  interface TerrestreInterface {
+    public function andar();
+  }
+
+  interface AquaticoInterface {
+    public function nadar();
+  }
+
+  class Humano implements MamiferoInterface, TerrestreInterface {
+
+    public function andar(){
+      echo 'Andar';
+    }
+
+    public function respirar(){
+      echo 'Respirar';
+    }
+
+    public function conversar(){
+      echo 'Conversar';
+    }
+  }
+
+  class Baleia implements MamiferoInterface, AquaticoInterface {
+
+    public function respirar(){
+      echo 'Respirar';
+    }
+
+    public function nadar(){
+      echo 'Nadar';
+    }
+
+    protected function esguichar(){
+      echo 'Esguichando';
+    }
+  }
+
+  $a = new Humano();
+  $a->respirar();
+  echo '<br>';
+  $b = new Baleia();
+  $b->nadar();
+
+  //------------------------------------------------------------
+
+  echo '<hr>';
+
+  interface AnimalInterface {
+    public function comer();
+  }
+
+  interface AveInterface extends AnimalInterface {
+    public function voar();
+  }
+
+  class Papagaio implements AveInterface {
+    public function voar(){
+      echo 'Voando';
+    }
+    public function comer(){
+      echo 'Comendo';
+    }
+  }
   
+  $z = new Papagaio();
+  $z->voar();
+
+
+
 
   ?>
 
