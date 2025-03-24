4c1662abcc3be3f5b7d1e1dbc2dd9003039c6e02
Soren <danilo-258@hotmail.com>
2025-03-24 15:57:57 -0300

    feat(promoção): adicionar página de demonstração de propriedade de construtor
    - Cria arquivo HTML com estrutura básica e título "Promoção de propriedade de construtor" para exemplificar a feature em PHP.

---
+++ class/novas_features/promocao_propriedade_construtor.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../php.css">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <h1>Promoção de propriedade de construtor</h1>
+  <?php
+
+  ?>
+
+</body>
+</html>
